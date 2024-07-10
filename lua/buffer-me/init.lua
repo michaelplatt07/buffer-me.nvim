a215b3b1d0e6521b63d562c80788ff20a1dd0ce4
michaelplatt07 <michael@lugia.lugia>
2024-07-10 08:52:56 -0600

    Window management updated * Adding a number of function calls to the init so they are accessible   to the vim file to be called in association with key commands * Swapping windower for a generalized module to follow plugin design   pattern and separation of logic

--- lua/buffer-me/init.lua
+++ lua/buffer-me/init.lua
@@ -1,16 +1,30 @@
-local windower = require("buffer-me.windower")
+local bufferme = require("buffer-me.bufferme")
+local keybindings = require("buffer-me.keybindings")
 local M = {}
 
 function M.open()
-	windower.create_floating_window()
+	-- Opens the buffer management list
+	bufferme.open_buffers_list()
+end
+
+function M.add()
+	-- Add teh current buffer to the end of the buffer management list
+	bufferme.add_buf()
+end
+
+function M.add_all()
+	-- Adds all current buffers open to the list
+	-- TODO(map) This shoud only add up to the first 10 buffers
 end
 
 function M.set_buffer_number()
 	-- Sets the current buffer to the given number in the list
+	bufferme.add_buf_at_idx()
 end
 
 function M.remove_buffer()
 	-- Removes the specified buffer from the list of managed buffers
+	bufferme.remove_buf_at_idx()
 end
 
 function M.go_to_buffer()
@@ -25,18 +39,22 @@ function M.prev_buffer()
 	-- Goes to previous buffer in the buffer set list
 end
 
+function M.clear_list()
+	-- Clears all the buffers from the lists
+end
+
 function M.toggle_last_buffer()
 	-- Toggles between the two most recent opened buffers
 end
 
 function M.setup(config)
-	-- if config ~= nil then
-	-- 	if config.keys ~= nil then
-	-- 		for func, custombind in pairs(config.keys) do
-	-- 			keybindings.update_key_binding(func, custombind)
-	-- 		end
-	-- 	end
-	-- end
+	if config ~= nil then
+		if config.keys ~= nil then
+			for func, custombind in pairs(config.keys) do
+				keybindings.update_key_binding(func, custombind)
+			end
+		end
+	end
 end
 
 return M
--- lua/buffer-me/windower.lua
+++ lua/buffer-me/windower.lua
@@ -1,12 +1,13 @@
+local state = require("buffer-me.state")
 local windower = {}
 
 function windower.create_floating_window()
-	return vim.api.nvim_open_win(0, true, {
+	return vim.api.nvim_open_win(state.bufListBuf, true, {
 		relative = "editor",
 		row = 0,
 		col = 0,
 		width = 100,
-		height = 100,
+		height = 20,
 		border = "double",
 		style = "minimal",
 		title = "Buffers",
@@ -15,16 +16,11 @@ end
 
 function windower.close_window()
 	-- Reset modifiable flag so the buffer can be updated on the next search
-	-- vim.api.nvim_buf_set_option(state.referenceBuf, "modifiable", true)
-	--
-	-- -- Close the buffers and recreate them
-	-- vim.api.nvim_buf_delete(state.referenceBuf, { force = true })
-	-- state.referenceBuf = vim.api.nvim_create_buf(false, true)
-	-- vim.api.nvim_buf_delete(state.previewBuf, { force = true })
-	-- state.previewBuf = vim.api.nvim_create_buf(false, true)
-	--
-	-- -- Clear the state for the next time it loads
-	-- state.clear_state()
+	vim.api.nvim_buf_set_option(state.bufListBuf, "modifiable", true)
+
+	-- Close the buffers and recreate them
+	vim.api.nvim_buf_delete(state.bufListBuf, { force = true })
+	state.bufListBuf = vim.api.nvim_create_buf(false, true)
 end
 
 return windower
