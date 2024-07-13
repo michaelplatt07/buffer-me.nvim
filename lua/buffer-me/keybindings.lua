9b58112b0619a7c935c0809bac24c5904843837f
michaelplatt07 <michael@lugia.lugia>
2024-07-13 17:14:48 -0600

    Adding key binding for opening buffer at index command

--- lua/buffer-me/keybindings.lua
+++ lua/buffer-me/keybindings.lua
@@ -6,6 +6,7 @@ function keybindings.update_key_binding(func, custombind) end
 
 function keybindings.map_keys(buf)
 	vim.api.nvim_buf_set_keymap(buf, "n", "o", ':lua require("buffer-me.bufferme").open_selected_buffer()<CR>', {})
+	vim.api.nvim_buf_set_keymap(buf, "n", "g", ':lua require("buffer-me.bufferme").go_to_buffer()<CR>', {})
 	vim.api.nvim_buf_set_keymap(buf, "n", "q", ':lua require("buffer-me.windower").close_window()<CR>', {})
 end
 
